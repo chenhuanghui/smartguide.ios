20eb81d9370f0e380111c002c8c2a010a0e05a65
MacMini <macmini@MacMinis-Mac-mini.local>
2013-12-05 23:00:04 +0700

    add button agree

---
+++ MakeButtonAgree/MakeButtonAgree/ButtonAgree.m
@@ -0,0 +1,31 @@
+//
+//  ButtonAgree.m
+//  MakeButtonAgree
+//
+//  Created by MacMini on 05/12/2013.
+//  Copyright (c) 2013 MacMini. All rights reserved.
+//
+
+#import "ButtonAgree.h"
+
+@implementation ButtonAgree
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ MakeButtonAgree/MakeButtonAgree/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MakeButtonAgree
+//
+//  Created by MacMini on 05/12/2013.
+//  Copyright (c) 2013 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
