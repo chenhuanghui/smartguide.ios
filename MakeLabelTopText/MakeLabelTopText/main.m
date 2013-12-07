8107170238d10161a4c00ca0fd45e2c9d5e5bd08
MacMini <macmini@MacMinis-Mac-mini.local>
2013-12-07 18:23:08 +0700

    add project make label top text

---
+++ MakeLabelTopText/MakeLabelTopText/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MakeLabelTopText
+//
+//  Created by MacMini on 07/12/2013.
+//  Copyright (c) 2013 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
