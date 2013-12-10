1b4e79843bc438eefee7d2eb4eb4b316e8ccbfed
MacMini <macmini@MacMinis-Mac-mini.local>
2013-12-10 09:15:46 +0700

    add utility scrollview

---
+++ SGScrollView/SGScrollView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SGScrollView
+//
+//  Created by MacMini on 10/12/2013.
+//  Copyright (c) 2013 MacMini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SGScrollView/SGScrollViewTests/SGScrollViewTests.m
@@ -0,0 +1,34 @@
+//
+//  SGScrollViewTests.m
+//  SGScrollViewTests
+//
+//  Created by MacMini on 10/12/2013.
+//  Copyright (c) 2013 MacMini. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SGScrollViewTests : XCTestCase
+
+@end
+
+@implementation SGScrollViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
