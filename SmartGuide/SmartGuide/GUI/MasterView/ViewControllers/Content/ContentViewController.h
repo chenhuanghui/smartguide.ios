288691027511a1b98718a5ceb062174c903fd30b
XXX <chitrung.it@gmail.com>
2013-12-31 15:05:23 +0700

    prepare update promotion shop user

---
+++ SmartGuide/SmartGuide/GUI/MasterView/ViewControllers/Content/ContentViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  SmartGuide
+//
+//  Created by MacMini on 25/10/2013.
+//  Copyright (c) 2013 Redbase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SGNavigationController.h"
+
+@protocol ContentViewDelegate <NSObject>
+
+@end
+
+@interface ContentViewController : SGNavigationController
+
+@end
